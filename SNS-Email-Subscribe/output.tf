3ead5ea8f8918e0166270b2a2e9a6aae8d403608
kgotlelelo-cmd <kmasenam@student.wethinkcode.co.za>
2023-12-12 22:55:21 +0200

    subscribe to sns topic

---
+++ SNS-Email-Subscribe/output.tf
@@ -0,0 +1,9 @@
+output "topic" {
+  value = aws_sns_topic.sns_topic.arn
+  description = "Topic created successfully"
+}
+
+output "subscription_arn1" {
+  value = aws_sns_topic_subscription.sns_subscripton.arn
+  description = "Subscription created successfully. Confirm the subscription on your mail"
+}
---
+++ SNS-Email-Subscribe/variables.tf
@@ -0,0 +1,16 @@
+variable "access_key" {
+  description = "Access key to AWS console"
+}
+
+variable "secret_key" {
+  description = "Secret key to AWS console"
+}
+
+variable "region" {
+  description = "Region of AWS VPC"
+}
+
+variable "sns_subscription_email" {
+    type = string
+  description = "Email endpoint for the SNS subscription"
+}
