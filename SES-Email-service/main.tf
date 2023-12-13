352f44fc51c73ef74927128470c903237736d7eb
kgotlelelo-cmd <kmasenam@student.wethinkcode.co.za>
2023-12-13 19:38:10 +0200

    SES config

---
+++ SES-Email-service/main.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+    region     = "${var.region}"
+    access_key = "${var.access_key}"
+    secret_key = "${var.secret_key}"
+}
+
+resource "aws_ses_email_identity" "ses_identity" {
+  email = "kgotlelelomasenamela74@gmail.com"
+}
---
+++ SES-Email-service/output.tf
@@ -0,0 +1,4 @@
+output "Identity_arn" {
+    value = aws_ses_email_identity.ses_identity.arn
+    description = "Identity created successfully"
+}
